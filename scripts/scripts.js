80994507e365a3ecbcce30495058145aa11a8b13
Jonathan Carlton <carlton.jonathan@icloud.com>
2023-11-14 14:06:24 -0600

    feat: Requirements. Outlining some of the requirments of the project.

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+// need function to select the computers choice
+// this need to be a randomized 1 in 3 chance to select either Rock (1) Paper (2) or Scissors (3)
+// we can use a random number generator and a swich case statement to assign each of the values
+
+// need a function to get the users input. 
+// user will be prompted to enter their selection. Either Rock (1), Paper (2), or Scissors (3).
+// this will need to account for both lowercase and uppercase characters
+
+// with the computers choice selected and the players choise selected, we need to compare the two to determine which one is the winner
+// the rules are, Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.
+// in the event that both players select the same choice it will be a draw and there will be no winner.
+
+// add a button allowing the player to play again and choose a new selection. This will also trigger the computer to roll for a new selection.
+
+// Extra: Could include a tally system, that could be used to keep track of the number of wins. ex best 2 of 3, best 3 of 5 etc.
+// Would also need a reset button to reset the current score (or reset on page refresh)
