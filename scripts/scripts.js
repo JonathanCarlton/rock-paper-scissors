9be2594acabc914e280d673a08edbd4a2835a5cd
Jonathan Carlton <carlton.jonathan@icloud.com>
2023-11-14 14:24:03 -0600

    Feat: Adding Game Functions. Added function for selecting computer choice and comparing with player

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,3 +14,56 @@
 
 // Extra: Could include a tally system, that could be used to keep track of the number of wins. ex best 2 of 3, best 3 of 5 etc.
 // Would also need a reset button to reset the current score (or reset on page refresh)
+
+function getRandomPositiveInt(max){
+    return Math.floor(Math.random() * max) + 1;
+}
+
+function getComputerChoice() {
+    let randomNumber = getRandomPositiveInt(3);
+    switch (randomNumber){
+        case 1:
+            return "rock"
+        case 2:
+            return "paper"
+        case 3:
+            return "scissors"
+    }
+}
+
+function playRockPaperScissors(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection === "rock") {
+                return "Draw - You both selected Rock";
+            }
+            if (computerSelection === "paper") {
+                return "You Lose! Paper beats Rock";
+            }
+            if (computerSelection === "scissors") {
+                return "You Win! Rock beats Scissors";
+            }
+
+        case "paper":
+            if (computerSelection === "rock") {
+                return "You Win! Paper beats Rock";
+            }
+            if (computerSelection === "paper") {
+                return "Draw! You both selected Paper";
+            }
+            if (computerSelection === "scissors") {
+                return "You Lose! Scissors beats Paper";
+            }
+
+        case "scissors":
+            if (computerSelection === "rock") {
+                return "You Lose! Rock beats Scissors";
+            }
+            if (computerSelection === "paper") {
+                return "You Win! Scissors beats Paper";
+            }
+            if (computerSelection === "scissors") {
+                return "Draw! you both selected Scissors";
+            }
+    }
+}
